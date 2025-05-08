0f289094c1a157a677cedd02d4a7155b94f458e5
ankit <ankitkarapurkar444@gmail.com>
2025-05-07 22:22:59 -0700

    latest  commit

--- src/components/StartRattig.jsx
+++ src/components/StartRattig.jsx
@@ -5,7 +5,7 @@ const StartRattig = ({rating=4}) => {
   return (
     <div className='flex gap-2'>
     {Array(5).fill('').map((_, index) => (
-     <img src={rating > index ? assets.starIconFilled :assets.starIconOutlined} alt="" className='w-4.5 h-4.5' />
+     <img key={index} src={rating > index ? assets.starIconFilled :assets.starIconOutlined} alt="" className='w-4.5 h-4.5' />
     ))}
     </div>
   )
